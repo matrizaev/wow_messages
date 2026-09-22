63f25a888bc70b0f29b7d72f40274b71e2e76e0b
Gtker <github@gtker.com>
2026-09-22 14:51:09 +0200

    WIP commit that will fail CI

--- wow_login_messages/src/logon/version_2/opcodes.rs
+++ wow_login_messages/src/logon/version_2/opcodes.rs
@@ -1,8 +1,8 @@
-use crate::{ServerMessage, ClientMessage};
-use crate::Message;
-use std::io::{Read, Write};
 use super::*;
 use crate::all::*;
+use crate::Message;
+use crate::{ClientMessage, ServerMessage};
+use std::io::{Read, Write};
 #[derive(Debug, Clone, PartialEq)]
 pub enum ClientOpcodeMessage {
     CMD_AUTH_LOGON_CHALLENGE(CMD_AUTH_LOGON_CHALLENGE_Client),
@@ -11,6 +11,7 @@ pub enum ClientOpcodeMessage {
     CMD_AUTH_RECONNECT_PROOF(CMD_AUTH_RECONNECT_PROOF_Client),
     CMD_REALM_LIST(CMD_REALM_LIST_Client),
     CMD_XFER_ACCEPT,
+
     CMD_XFER_RESUME(CMD_XFER_RESUME),
     CMD_XFER_CANCEL,
 }
@@ -37,50 +38,97 @@ impl ClientOpcodeMessage {
     pub fn read<R: std::io::Read>(mut r: R) -> Result<Self, crate::errors::ExpectedOpcodeError> {
         let opcode = crate::util::read_u8_le(&mut r)?;
         match opcode {
-            0x00 => Ok(Self::CMD_AUTH_LOGON_CHALLENGE(CMD_AUTH_LOGON_CHALLENGE_Client::read::<R, crate::private::Internal>(r)?)),
-            0x01 => Ok(Self::CMD_AUTH_LOGON_PROOF(CMD_AUTH_LOGON_PROOF_Client::read::<R, crate::private::Internal>(r)?)),
-            0x02 => Ok(Self::CMD_AUTH_RECONNECT_CHALLENGE(CMD_AUTH_RECONNECT_CHALLENGE_Client::read::<R, crate::private::Internal>(r)?)),
-            0x03 => Ok(Self::CMD_AUTH_RECONNECT_PROOF(CMD_AUTH_RECONNECT_PROOF_Client::read::<R, crate::private::Internal>(r)?)),
-            0x10 => Ok(Self::CMD_REALM_LIST(CMD_REALM_LIST_Client::read::<R, crate::private::Internal>(r)?)),
+            0x00 => Ok(Self::CMD_AUTH_LOGON_CHALLENGE(
+                CMD_AUTH_LOGON_CHALLENGE_Client::read::<R, crate::private::Internal>(r)?,
+            )),
+            0x01 => Ok(Self::CMD_AUTH_LOGON_PROOF(
+                CMD_AUTH_LOGON_PROOF_Client::read::<R, crate::private::Internal>(r)?,
+            )),
+            0x02 => Ok(Self::CMD_AUTH_RECONNECT_CHALLENGE(
+                CMD_AUTH_RECONNECT_CHALLENGE_Client::read::<R, crate::private::Internal>(r)?,
+            )),
+            0x03 => Ok(Self::CMD_AUTH_RECONNECT_PROOF(
+                CMD_AUTH_RECONNECT_PROOF_Client::read::<R, crate::private::Internal>(r)?,
+            )),
+            0x10 => Ok(Self::CMD_REALM_LIST(CMD_REALM_LIST_Client::read::<
+                R,
+                crate::private::Internal,
+            >(r)?)),
             0x32 => Ok(Self::CMD_XFER_ACCEPT),
-            0x33 => Ok(Self::CMD_XFER_RESUME(CMD_XFER_RESUME::read::<R, crate::private::Internal>(r)?)),
+            0x33 => Ok(Self::CMD_XFER_RESUME(CMD_XFER_RESUME::read::<
+                R,
+                crate::private::Internal,
+            >(r)?)),
             0x34 => Ok(Self::CMD_XFER_CANCEL),
             opcode => Err(crate::errors::ExpectedOpcodeError::Opcode(opcode as u32)),
         }
     }
 
     #[cfg(feature = "tokio")]
-    pub async fn tokio_read<R: tokio::io::AsyncReadExt + Unpin + Send>(mut r: R) -> Result<Self, crate::errors::ExpectedOpcodeError> {
+    pub async fn tokio_read<R: tokio::io::AsyncReadExt + Unpin + Send>(
+        mut r: R,
+    ) -> Result<Self, crate::errors::ExpectedOpcodeError> {
         let opcode = crate::util::tokio_read_u8_le(&mut r).await?;
         match opcode {
-            0x00 => Ok(Self::CMD_AUTH_LOGON_CHALLENGE(CMD_AUTH_LOGON_CHALLENGE_Client::tokio_read::<R, crate::private::Internal>(r).await?)),
-            0x01 => Ok(Self::CMD_AUTH_LOGON_PROOF(CMD_AUTH_LOGON_PROOF_Client::tokio_read::<R, crate::private::Internal>(r).await?)),
-            0x02 => Ok(Self::CMD_AUTH_RECONNECT_CHALLENGE(CMD_AUTH_RECONNECT_CHALLENGE_Client::tokio_read::<R, crate::private::Internal>(r).await?)),
-            0x03 => Ok(Self::CMD_AUTH_RECONNECT_PROOF(CMD_AUTH_RECONNECT_PROOF_Client::tokio_read::<R, crate::private::Internal>(r).await?)),
-            0x10 => Ok(Self::CMD_REALM_LIST(CMD_REALM_LIST_Client::tokio_read::<R, crate::private::Internal>(r).await?)),
+            0x00 => Ok(Self::CMD_AUTH_LOGON_CHALLENGE(
+                CMD_AUTH_LOGON_CHALLENGE_Client::tokio_read::<R, crate::private::Internal>(r)
+                    .await?,
+            )),
+            0x01 => Ok(Self::CMD_AUTH_LOGON_PROOF(
+                CMD_AUTH_LOGON_PROOF_Client::tokio_read::<R, crate::private::Internal>(r).await?,
+            )),
+            0x02 => Ok(Self::CMD_AUTH_RECONNECT_CHALLENGE(
+                CMD_AUTH_RECONNECT_CHALLENGE_Client::tokio_read::<R, crate::private::Internal>(r)
+                    .await?,
+            )),
+            0x03 => Ok(Self::CMD_AUTH_RECONNECT_PROOF(
+                CMD_AUTH_RECONNECT_PROOF_Client::tokio_read::<R, crate::private::Internal>(r)
+                    .await?,
+            )),
+            0x10 => Ok(Self::CMD_REALM_LIST(
+                CMD_REALM_LIST_Client::tokio_read::<R, crate::private::Internal>(r).await?,
+            )),
             0x32 => Ok(Self::CMD_XFER_ACCEPT),
-            0x33 => Ok(Self::CMD_XFER_RESUME(CMD_XFER_RESUME::tokio_read::<R, crate::private::Internal>(r).await?)),
+            0x33 => Ok(Self::CMD_XFER_RESUME(
+                CMD_XFER_RESUME::tokio_read::<R, crate::private::Internal>(r).await?,
+            )),
             0x34 => Ok(Self::CMD_XFER_CANCEL),
             opcode => Err(crate::errors::ExpectedOpcodeError::Opcode(opcode as u32)),
         }
     }
 
     #[cfg(feature = "async-std")]
-    pub async fn astd_read<R: async_std::io::ReadExt + Unpin + Send>(mut r: R) -> Result<Self, crate::errors::ExpectedOpcodeError> {
+    pub async fn astd_read<R: async_std::io::ReadExt + Unpin + Send>(
+        mut r: R,
+    ) -> Result<Self, crate::errors::ExpectedOpcodeError> {
         let opcode = crate::util::astd_read_u8_le(&mut r).await?;
         match opcode {
-            0x00 => Ok(Self::CMD_AUTH_LOGON_CHALLENGE(CMD_AUTH_LOGON_CHALLENGE_Client::astd_read::<R, crate::private::Internal>(r).await?)),
-            0x01 => Ok(Self::CMD_AUTH_LOGON_PROOF(CMD_AUTH_LOGON_PROOF_Client::astd_read::<R, crate::private::Internal>(r).await?)),
-            0x02 => Ok(Self::CMD_AUTH_RECONNECT_CHALLENGE(CMD_AUTH_RECONNECT_CHALLENGE_Client::astd_read::<R, crate::private::Internal>(r).await?)),
-            0x03 => Ok(Self::CMD_AUTH_RECONNECT_PROOF(CMD_AUTH_RECONNECT_PROOF_Client::astd_read::<R, crate::private::Internal>(r).await?)),
-            0x10 => Ok(Self::CMD_REALM_LIST(CMD_REALM_LIST_Client::astd_read::<R, crate::private::Internal>(r).await?)),
+            0x00 => Ok(Self::CMD_AUTH_LOGON_CHALLENGE(
+                CMD_AUTH_LOGON_CHALLENGE_Client::astd_read::<R, crate::private::Internal>(r)
+                    .await?,
+            )),
+            0x01 => Ok(Self::CMD_AUTH_LOGON_PROOF(
+                CMD_AUTH_LOGON_PROOF_Client::astd_read::<R, crate::private::Internal>(r).await?,
+            )),
+            0x02 => Ok(Self::CMD_AUTH_RECONNECT_CHALLENGE(
+                CMD_AUTH_RECONNECT_CHALLENGE_Client::astd_read::<R, crate::private::Internal>(r)
+                    .await?,
+            )),
+            0x03 => Ok(Self::CMD_AUTH_RECONNECT_PROOF(
+                CMD_AUTH_RECONNECT_PROOF_Client::astd_read::<R, crate::private::Internal>(r)
+                    .await?,
+            )),
+            0x10 => Ok(Self::CMD_REALM_LIST(
+                CMD_REALM_LIST_Client::astd_read::<R, crate::private::Internal>(r).await?,
+            )),
             0x32 => Ok(Self::CMD_XFER_ACCEPT),
-            0x33 => Ok(Self::CMD_XFER_RESUME(CMD_XFER_RESUME::astd_read::<R, crate::private::Internal>(r).await?)),
+            0x33 => Ok(Self::CMD_XFER_RESUME(
+                CMD_XFER_RESUME::astd_read::<R, crate::private::Internal>(r).await?,
+            )),
             0x34 => Ok(Self::CMD_XFER_CANCEL),
             opcode => Err(crate::errors::ExpectedOpcodeError::Opcode(opcode as u32)),
         }
     }
-
 }
 
 impl std::fmt::Display for ClientOpcodeMessage {
@@ -88,7 +136,9 @@ impl std::fmt::Display for ClientOpcodeMessage {
         f.write_str(match self {
             ClientOpcodeMessage::CMD_AUTH_LOGON_CHALLENGE(_) => "CMD_AUTH_LOGON_CHALLENGE_Client",
             ClientOpcodeMessage::CMD_AUTH_LOGON_PROOF(_) => "CMD_AUTH_LOGON_PROOF_Client",
-            ClientOpcodeMessage::CMD_AUTH_RECONNECT_CHALLENGE(_) => "CMD_AUTH_RECONNECT_CHALLENGE_Client",
+            ClientOpcodeMessage::CMD_AUTH_RECONNECT_CHALLENGE(_) => {
+                "CMD_AUTH_RECONNECT_CHALLENGE_Client"
+            }
             ClientOpcodeMessage::CMD_AUTH_RECONNECT_PROOF(_) => "CMD_AUTH_RECONNECT_PROOF_Client",
             ClientOpcodeMessage::CMD_REALM_LIST(_) => "CMD_REALM_LIST_Client",
             ClientOpcodeMessage::CMD_XFER_ACCEPT => "CMD_XFER_ACCEPT",
@@ -178,47 +228,101 @@ impl ServerOpcodeMessage {
     pub fn read<R: std::io::Read>(mut r: R) -> Result<Self, crate::errors::ExpectedOpcodeError> {
         let opcode = crate::util::read_u8_le(&mut r)?;
         match opcode {
-            0x00 => Ok(Self::CMD_AUTH_LOGON_CHALLENGE(CMD_AUTH_LOGON_CHALLENGE_Server::read::<R, crate::private::Internal>(r)?)),
-            0x01 => Ok(Self::CMD_AUTH_LOGON_PROOF(CMD_AUTH_LOGON_PROOF_Server::read::<R, crate::private::Internal>(r)?)),
-            0x02 => Ok(Self::CMD_AUTH_RECONNECT_CHALLENGE(CMD_AUTH_RECONNECT_CHALLENGE_Server::read::<R, crate::private::Internal>(r)?)),
-            0x03 => Ok(Self::CMD_AUTH_RECONNECT_PROOF(CMD_AUTH_RECONNECT_PROOF_Server::read::<R, crate::private::Internal>(r)?)),
-            0x10 => Ok(Self::CMD_REALM_LIST(CMD_REALM_LIST_Server::read::<R, crate::private::Internal>(r)?)),
-            0x30 => Ok(Self::CMD_XFER_INITIATE(CMD_XFER_INITIATE::read::<R, crate::private::Internal>(r)?)),
-            0x31 => Ok(Self::CMD_XFER_DATA(CMD_XFER_DATA::read::<R, crate::private::Internal>(r)?)),
+            0x00 => Ok(Self::CMD_AUTH_LOGON_CHALLENGE(
+                CMD_AUTH_LOGON_CHALLENGE_Server::read::<R, crate::private::Internal>(r)?,
+            )),
+            0x01 => Ok(Self::CMD_AUTH_LOGON_PROOF(
+                CMD_AUTH_LOGON_PROOF_Server::read::<R, crate::private::Internal>(r)?,
+            )),
+            0x02 => Ok(Self::CMD_AUTH_RECONNECT_CHALLENGE(
+                CMD_AUTH_RECONNECT_CHALLENGE_Server::read::<R, crate::private::Internal>(r)?,
+            )),
+            0x03 => Ok(Self::CMD_AUTH_RECONNECT_PROOF(
+                CMD_AUTH_RECONNECT_PROOF_Server::read::<R, crate::private::Internal>(r)?,
+            )),
+            0x10 => Ok(Self::CMD_REALM_LIST(CMD_REALM_LIST_Server::read::<
+                R,
+                crate::private::Internal,
+            >(r)?)),
+            0x30 => Ok(Self::CMD_XFER_INITIATE(CMD_XFER_INITIATE::read::<
+                R,
+                crate::private::Internal,
+            >(r)?)),
+            0x31 => Ok(Self::CMD_XFER_DATA(CMD_XFER_DATA::read::<
+                R,
+                crate::private::Internal,
+            >(r)?)),
             opcode => Err(crate::errors::ExpectedOpcodeError::Opcode(opcode as u32)),
         }
     }
 
     #[cfg(feature = "tokio")]
-    pub async fn tokio_read<R: tokio::io::AsyncReadExt + Unpin + Send>(mut r: R) -> Result<Self, crate::errors::ExpectedOpcodeError> {
+    pub async fn tokio_read<R: tokio::io::AsyncReadExt + Unpin + Send>(
+        mut r: R,
+    ) -> Result<Self, crate::errors::ExpectedOpcodeError> {
         let opcode = crate::util::tokio_read_u8_le(&mut r).await?;
         match opcode {
-            0x00 => Ok(Self::CMD_AUTH_LOGON_CHALLENGE(CMD_AUTH_LOGON_CHALLENGE_Server::tokio_read::<R, crate::private::Internal>(r).await?)),
-            0x01 => Ok(Self::CMD_AUTH_LOGON_PROOF(CMD_AUTH_LOGON_PROOF_Server::tokio_read::<R, crate::private::Internal>(r).await?)),
-            0x02 => Ok(Self::CMD_AUTH_RECONNECT_CHALLENGE(CMD_AUTH_RECONNECT_CHALLENGE_Server::tokio_read::<R, crate::private::Internal>(r).await?)),
-            0x03 => Ok(Self::CMD_AUTH_RECONNECT_PROOF(CMD_AUTH_RECONNECT_PROOF_Server::tokio_read::<R, crate::private::Internal>(r).await?)),
-            0x10 => Ok(Self::CMD_REALM_LIST(CMD_REALM_LIST_Server::tokio_read::<R, crate::private::Internal>(r).await?)),
-            0x30 => Ok(Self::CMD_XFER_INITIATE(CMD_XFER_INITIATE::tokio_read::<R, crate::private::Internal>(r).await?)),
-            0x31 => Ok(Self::CMD_XFER_DATA(CMD_XFER_DATA::tokio_read::<R, crate::private::Internal>(r).await?)),
+            0x00 => Ok(Self::CMD_AUTH_LOGON_CHALLENGE(
+                CMD_AUTH_LOGON_CHALLENGE_Server::tokio_read::<R, crate::private::Internal>(r)
+                    .await?,
+            )),
+            0x01 => Ok(Self::CMD_AUTH_LOGON_PROOF(
+                CMD_AUTH_LOGON_PROOF_Server::tokio_read::<R, crate::private::Internal>(r).await?,
+            )),
+            0x02 => Ok(Self::CMD_AUTH_RECONNECT_CHALLENGE(
+                CMD_AUTH_RECONNECT_CHALLENGE_Server::tokio_read::<R, crate::private::Internal>(r)
+                    .await?,
+            )),
+            0x03 => Ok(Self::CMD_AUTH_RECONNECT_PROOF(
+                CMD_AUTH_RECONNECT_PROOF_Server::tokio_read::<R, crate::private::Internal>(r)
+                    .await?,
+            )),
+            0x10 => Ok(Self::CMD_REALM_LIST(
+                CMD_REALM_LIST_Server::tokio_read::<R, crate::private::Internal>(r).await?,
+            )),
+            0x30 => Ok(Self::CMD_XFER_INITIATE(
+                CMD_XFER_INITIATE::tokio_read::<R, crate::private::Internal>(r).await?,
+            )),
+            0x31 => Ok(Self::CMD_XFER_DATA(
+                CMD_XFER_DATA::tokio_read::<R, crate::private::Internal>(r).await?,
+            )),
             opcode => Err(crate::errors::ExpectedOpcodeError::Opcode(opcode as u32)),
         }
     }
 
     #[cfg(feature = "async-std")]
-    pub async fn astd_read<R: async_std::io::ReadExt + Unpin + Send>(mut r: R) -> Result<Self, crate::errors::ExpectedOpcodeError> {
+    pub async fn astd_read<R: async_std::io::ReadExt + Unpin + Send>(
+        mut r: R,
+    ) -> Result<Self, crate::errors::ExpectedOpcodeError> {
         let opcode = crate::util::astd_read_u8_le(&mut r).await?;
         match opcode {
-            0x00 => Ok(Self::CMD_AUTH_LOGON_CHALLENGE(CMD_AUTH_LOGON_CHALLENGE_Server::astd_read::<R, crate::private::Internal>(r).await?)),
-            0x01 => Ok(Self::CMD_AUTH_LOGON_PROOF(CMD_AUTH_LOGON_PROOF_Server::astd_read::<R, crate::private::Internal>(r).await?)),
-            0x02 => Ok(Self::CMD_AUTH_RECONNECT_CHALLENGE(CMD_AUTH_RECONNECT_CHALLENGE_Server::astd_read::<R, crate::private::Internal>(r).await?)),
-            0x03 => Ok(Self::CMD_AUTH_RECONNECT_PROOF(CMD_AUTH_RECONNECT_PROOF_Server::astd_read::<R, crate::private::Internal>(r).await?)),
-            0x10 => Ok(Self::CMD_REALM_LIST(CMD_REALM_LIST_Server::astd_read::<R, crate::private::Internal>(r).await?)),
-            0x30 => Ok(Self::CMD_XFER_INITIATE(CMD_XFER_INITIATE::astd_read::<R, crate::private::Internal>(r).await?)),
-            0x31 => Ok(Self::CMD_XFER_DATA(CMD_XFER_DATA::astd_read::<R, crate::private::Internal>(r).await?)),
+            0x00 => Ok(Self::CMD_AUTH_LOGON_CHALLENGE(
+                CMD_AUTH_LOGON_CHALLENGE_Server::astd_read::<R, crate::private::Internal>(r)
+                    .await?,
+            )),
+            0x01 => Ok(Self::CMD_AUTH_LOGON_PROOF(
+                CMD_AUTH_LOGON_PROOF_Server::astd_read::<R, crate::private::Internal>(r).await?,
+            )),
+            0x02 => Ok(Self::CMD_AUTH_RECONNECT_CHALLENGE(
+                CMD_AUTH_RECONNECT_CHALLENGE_Server::astd_read::<R, crate::private::Internal>(r)
+                    .await?,
+            )),
+            0x03 => Ok(Self::CMD_AUTH_RECONNECT_PROOF(
+                CMD_AUTH_RECONNECT_PROOF_Server::astd_read::<R, crate::private::Internal>(r)
+                    .await?,
+            )),
+            0x10 => Ok(Self::CMD_REALM_LIST(
+                CMD_REALM_LIST_Server::astd_read::<R, crate::private::Internal>(r).await?,
+            )),
+            0x30 => Ok(Self::CMD_XFER_INITIATE(
+                CMD_XFER_INITIATE::astd_read::<R, crate::private::Internal>(r).await?,
+            )),
+            0x31 => Ok(Self::CMD_XFER_DATA(
+                CMD_XFER_DATA::astd_read::<R, crate::private::Internal>(r).await?,
+            )),
             opcode => Err(crate::errors::ExpectedOpcodeError::Opcode(opcode as u32)),
         }
     }
-
 }
 
 impl std::fmt::Display for ServerOpcodeMessage {
@@ -226,7 +330,9 @@ impl std::fmt::Display for ServerOpcodeMessage {
         f.write_str(match self {
             ServerOpcodeMessage::CMD_AUTH_LOGON_CHALLENGE(_) => "CMD_AUTH_LOGON_CHALLENGE_Server",
             ServerOpcodeMessage::CMD_AUTH_LOGON_PROOF(_) => "CMD_AUTH_LOGON_PROOF_Server",
-            ServerOpcodeMessage::CMD_AUTH_RECONNECT_CHALLENGE(_) => "CMD_AUTH_RECONNECT_CHALLENGE_Server",
+            ServerOpcodeMessage::CMD_AUTH_RECONNECT_CHALLENGE(_) => {
+                "CMD_AUTH_RECONNECT_CHALLENGE_Server"
+            }
             ServerOpcodeMessage::CMD_AUTH_RECONNECT_PROOF(_) => "CMD_AUTH_RECONNECT_PROOF_Server",
             ServerOpcodeMessage::CMD_REALM_LIST(_) => "CMD_REALM_LIST_Server",
             ServerOpcodeMessage::CMD_XFER_INITIATE(_) => "CMD_XFER_INITIATE",
@@ -276,4 +382,3 @@ impl From<CMD_XFER_DATA> for ServerOpcodeMessage {
         Self::CMD_XFER_DATA(c)
     }
 }
-
